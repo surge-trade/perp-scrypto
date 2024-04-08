0607e5f14e38a10dcc97f4efd436782300c0e75c
FR05T8YTE <frostbyte058@gmail.com>
2024-04-08 07:20:13 +0000

    Update scripts

--- deploy/margin_order_request.py
+++ deploy/margin_order_request.py
@@ -46,15 +46,15 @@ async def main():
             ret.ManifestBuilderAddress.STATIC(ret.Address(exchange_component)),
             'margin_order_request',
             [
-                ret.ManifestBuilderValue.ENUM_VALUE(0, []),
-                ret.ManifestBuilderValue.U64_VALUE(10000000000),
-                ret.ManifestBuilderValue.ADDRESS_VALUE(ret.ManifestBuilderAddress.STATIC(ret.Address(account_component))),
-                ret.ManifestBuilderValue.U16_VALUE(1),
-                ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('1')),
-                ret.ManifestBuilderValue.ENUM_VALUE(0, [ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('0'))]),
-                ret.ManifestBuilderValue.ARRAY_VALUE(ret.ManifestBuilderValueKind.U64_VALUE, []),
-                ret.ManifestBuilderValue.ARRAY_VALUE(ret.ManifestBuilderValueKind.U64_VALUE, []),
-                ret.ManifestBuilderValue.U8_VALUE(1),
+                ret.ManifestBuilderValue.ENUM_VALUE(0, []), # Fee oath
+                ret.ManifestBuilderValue.U64_VALUE(10000000000), # Expiry seconds
+                ret.ManifestBuilderValue.ADDRESS_VALUE(ret.ManifestBuilderAddress.STATIC(ret.Address(account_component))), # Margin account
+                ret.ManifestBuilderValue.U16_VALUE(0), # Pair id
+                ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('0.000001')), # Amount
+                ret.ManifestBuilderValue.ENUM_VALUE(0, [ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('76000'))]), # Price limit
+                ret.ManifestBuilderValue.ARRAY_VALUE(ret.ManifestBuilderValueKind.U64_VALUE, []), # Activate requests
+                ret.ManifestBuilderValue.ARRAY_VALUE(ret.ManifestBuilderValueKind.U64_VALUE, []), # Cancel requests
+                ret.ManifestBuilderValue.U8_VALUE(1), # Status
             ]
         )
 
