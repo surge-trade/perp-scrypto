62eaf2703fc2244b6ad39d70556fb73bc595bd5b
FR05T8YTE <frostbyte058@gmail.com>
2024-04-17 05:45:27 +0000

    Release

--- exchange/src/exchange.rs
+++ exchange/src/exchange.rs
@@ -36,7 +36,7 @@ mod exchange {
     const KEEPER_REWARD_RESOURCE: ResourceAddress = _KEEPER_REWARD_RESOURCE;
 
     extern_blueprint! {
-        "package_tdx_2_1p47aw6da6mzl8vptcxyhva43qv80zvdnl7pqjxlnkru7jkx8xnpyev",
+        "package_tdx_2_1phxrzlgxkvzpkddr8xm7lxyyfr68ev7n40zwnmcrsk0ah8wmy5a25t",
         Config {
             // Constructor
             fn new(initial_rule: AccessRule) -> Global<MarginAccount>;
@@ -56,7 +56,7 @@ mod exchange {
         }
     }
     extern_blueprint! {
-        "package_tdx_2_1p54h2g0rx3z9aplazenqq0cwkm8zrrd88699hdtyy28hxhm629lxvu",
+        "package_tdx_2_1p420rf2ms0y5lr8m4ps5qjggk3r2nw2jkz2x8fgggn2vw2xxaav5v3",
         MarginAccount {
             // Constructor
             fn new(initial_rule: AccessRule, reservation: Option<GlobalAddressReservation>) -> Global<MarginAccount>;
@@ -76,7 +76,7 @@ mod exchange {
         }
     }
     extern_blueprint! {
-        "package_tdx_2_1p4s3xqgyzrz5mhnga44ycjfuulfm7fhh68tktykfa3zfslha3d8sc2",
+        "package_tdx_2_1p5vlv28ymprxwguz6a46kdtr2d58v2mwc9fr0nw2el5tpdkm7xpavh",
         MarginPool {
             // Getter methods
             fn get_info(&self, pair_ids: HashSet<PairId>) -> MarginPoolInfo;
@@ -90,14 +90,14 @@ mod exchange {
         }
     }
     extern_blueprint! {
-        "package_tdx_2_1p4w7kfrl3273meyg2x2z05qaaxgm6q6002w68tj3wkenaefmezyk44",
+        "package_tdx_2_1p5h2tpageyhtuu4qft7sk7gmmj7nwfhcsc29qvs7cj0caug2wnpgtm",
         Oracle {
             // Getter methods
             fn prices(&self, max_age: Instant) -> HashMap<PairId, Decimal>;
         }
     }
     extern_blueprint! {
-        "package_tdx_2_1pk4d6a6sly02cd3j2nqw3puhjam4prn3c0ys89ldne7fx8srn6wyqq",
+        "package_tdx_2_1p42tgsv64mvfasmrp349kt8e3t25cu4he2ktuu0zwehnpdf30tjfj4",
         FeeDistributor {
             // Getter methods
             fn get_referrer(&self, account: ComponentAddress) -> Option<ComponentAddress>;
@@ -117,7 +117,7 @@ mod exchange {
         }
     }
     extern_blueprint! {
-        "package_tdx_2_1p4tzkrw399eedxmz7ejsadt0mswkyt5xl57eteywy2p4fp799wcnec",
+        "package_tdx_2_1p5zp72ehqqy20enz7umd04wcxe3zg3lraaucp77dsxy79u3l0w6p6m",
         FeeDelegator {
             // Getter methods
             fn get_fee_oath_resource(&self) -> ResourceAddress;
