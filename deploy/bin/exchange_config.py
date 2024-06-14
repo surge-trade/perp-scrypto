5c12f7b2fcfe007f075a26d38456dc9730d99067
FR05T8YTE <frostbyte058@gmail.com>
2024-06-14 09:33:45 +0000

    Scripts

--- deploy/bin/exchange_config.py
+++ deploy/bin/exchange_config.py
@@ -30,7 +30,6 @@ async def main():
         config_path = join(path, 'config.json')
         with open(config_path, 'r') as config_file:
             config_data = json.load(config_file)
-        print('Config loaded:', config_data)
 
         owner_resource = config_data['OWNER_RESOURCE']
         exchange_component = config_data['EXCHANGE_COMPONENT']
@@ -64,7 +63,7 @@ async def main():
                     ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('0.005')), # fee_liquidity
                     ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('0.1')), # fee_share_protocol
                     ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('0.1')), # fee_share_treasury
-                    ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('1')), # fee_share_referral
+                    ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('0.15')), # fee_share_referral
                     ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('0.01')), # fee_max
                     ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('1000')), # protocol_burn_amount
                     ret.ManifestBuilderValue.DECIMAL_VALUE(ret.Decimal('1')), # reward_keeper
