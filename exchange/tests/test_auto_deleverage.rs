85469df615153113b8359cdc33630d8edb1fec34
FR05T8YTE <frostbyte058@gmail.com>
2024-08-23 12:10:58 +0700

    Tests

--- exchange/tests/test_auto_deleverage.rs
+++ exchange/tests/test_auto_deleverage.rs
@@ -175,3 +175,384 @@ fn test_auto_deleverage() {
     assert_eq!(event.pnl_percent, pnl_percent);
     assert_eq!(event.threshold, threshold);
 }
+
+#[test]
+fn test_auto_deleverage_skew_too_low() {
+    let mut interface = get_setup();
+    let base_resource = interface.resources.base_resource;
+    let referral_resource = interface.resources.referral_resource;
+    
+    let pair_config = PairConfig {
+        pair_id: "BTC/USD".into(),
+        oi_max: dec!(100000),
+        update_price_delta_ratio: dec!(0.005),
+        update_period_seconds: 3600,
+        margin_initial: dec!(0.01),
+        margin_maintenance: dec!(0.005),
+        funding_1: dec!(0.0000000317),
+        funding_2: dec!(0.0000000317),
+        funding_2_delta: dec!(0.000000827),
+        funding_pool_0: dec!(0.0000000159),
+        funding_pool_1: dec!(0.0000000317),
+        funding_share: dec!(0.1),
+        fee_0: dec!(0.0005),
+        fee_1: dec!(0.0000000005),
+    };
+    interface.update_pair_configs(vec![pair_config.clone()]).expect_commit_success();
+
+    let fee_referral_0 = dec!(0.10);
+    let fee_rebate_0 = dec!(0.05);
+    let result_0 = interface.mint_referral(fee_referral_0, fee_rebate_0, 1).expect_commit_success().clone();
+    
+    let referral_id_1 = parse_added_nft_ids(&result_0, referral_resource).first().unwrap().clone();
+    let referral_code_1 = "test".to_string();
+    let referral_hash_1 = keccak256_hash(referral_code_1.clone().into_bytes());
+    let referral_hashes_1 = hashmap!(
+        referral_hash_1 => (vec![], 1u64),
+    );
+    interface.create_referral_codes((referral_resource, referral_id_1), vec![], referral_hashes_1).expect_commit_success();
+
+    let base_input_2 = dec!(100000);
+    interface.add_liquidity((base_resource, base_input_2)).expect_commit_success();
+
+    let base_input_3 = dec!(1000);
+    let result_3 = interface.create_account(
+        rule!(allow_all), 
+        vec![(base_resource, base_input_3)], 
+        Some(referral_code_1),
+    ).expect_commit_success().clone();
+    let margin_account_component = result_3.new_component_addresses()[0];
+
+    let trade_size_4 = dec!(0.02);
+    interface.margin_order_tp_sl_request(
+        0,
+        10000000000,
+        margin_account_component,
+        pair_config.pair_id.clone(),
+        trade_size_4,
+        false,
+        Limit::None,
+        None,
+        None,
+    ).expect_commit_success();
+
+    let price_5 = dec!(60000);
+    let amount_long_5 = dec!(0.2);
+    let amount_short_5 = dec!(0.1);
+    interface.make_open_interest(pair_config.pair_id.clone(), amount_long_5, amount_short_5, price_5);
+
+    let price_6 = dec!(60000);
+    let time_6 = interface.increment_ledger_time(1);
+    interface.process_request(
+        margin_account_component,
+        0, 
+        Some(vec![
+            Price {
+                pair: pair_config.pair_id.clone(),
+                quote: price_6,
+                timestamp: time_6,
+            },
+        ])
+    ).expect_commit_success();
+
+    let price_7 = dec!(75000);
+    let time_7 = interface.increment_ledger_time(10000);
+    interface.auto_deleverage(
+        margin_account_component,
+        pair_config.pair_id.clone(),
+        Some(vec![
+            Price {
+                pair: pair_config.pair_id.clone(),
+                quote: price_7,
+                timestamp: time_7,
+            },
+        ])
+    ).expect_specific_failure(|err| check_error_msg(err, ERROR_ADL_SKEW_TOO_LOW));
+}
+
+#[test]
+fn test_auto_deleverage_no_position() {
+    let mut interface = get_setup();
+    let base_resource = interface.resources.base_resource;
+    let referral_resource = interface.resources.referral_resource;
+    
+    let pair_config = PairConfig {
+        pair_id: "BTC/USD".into(),
+        oi_max: dec!(100000),
+        update_price_delta_ratio: dec!(0.005),
+        update_period_seconds: 3600,
+        margin_initial: dec!(0.01),
+        margin_maintenance: dec!(0.005),
+        funding_1: dec!(0.0000000317),
+        funding_2: dec!(0.0000000317),
+        funding_2_delta: dec!(0.000000827),
+        funding_pool_0: dec!(0.0000000159),
+        funding_pool_1: dec!(0.0000000317),
+        funding_share: dec!(0.1),
+        fee_0: dec!(0.0005),
+        fee_1: dec!(0.0000000005),
+    };
+    interface.update_pair_configs(vec![pair_config.clone()]).expect_commit_success();
+
+    let fee_referral_0 = dec!(0.10);
+    let fee_rebate_0 = dec!(0.05);
+    let result_0 = interface.mint_referral(fee_referral_0, fee_rebate_0, 1).expect_commit_success().clone();
+    
+    let referral_id_1 = parse_added_nft_ids(&result_0, referral_resource).first().unwrap().clone();
+    let referral_code_1 = "test".to_string();
+    let referral_hash_1 = keccak256_hash(referral_code_1.clone().into_bytes());
+    let referral_hashes_1 = hashmap!(
+        referral_hash_1 => (vec![], 1u64),
+    );
+    interface.create_referral_codes((referral_resource, referral_id_1), vec![], referral_hashes_1).expect_commit_success();
+
+    let base_input_2 = dec!(100000);
+    interface.add_liquidity((base_resource, base_input_2)).expect_commit_success();
+
+    let base_input_3 = dec!(1000);
+    let result_3 = interface.create_account(
+        rule!(allow_all), 
+        vec![(base_resource, base_input_3)], 
+        Some(referral_code_1),
+    ).expect_commit_success().clone();
+    let margin_account_component = result_3.new_component_addresses()[0];
+
+    let price_4 = dec!(60000);
+    let amount_long_4 = dec!(0.2);
+    let amount_short_4 = dec!(0.01);
+    interface.make_open_interest(pair_config.pair_id.clone(), amount_long_4, amount_short_4, price_4);
+
+    let price_5 = dec!(75000);
+    let time_5 = interface.increment_ledger_time(10000);
+    interface.auto_deleverage(
+        margin_account_component,
+        pair_config.pair_id.clone(),
+        Some(vec![
+            Price {
+                pair: pair_config.pair_id.clone(),
+                quote: price_5,
+                timestamp: time_5,
+            },
+        ])
+    ).expect_specific_failure(|err| check_error_msg(err, ERROR_ADL_NO_POSITION));
+}
+
+#[test]
+fn test_auto_deleverage_below_threshold() {
+    let mut interface = get_setup();
+    let base_resource = interface.resources.base_resource;
+    let referral_resource = interface.resources.referral_resource;
+    
+    let pair_config = PairConfig {
+        pair_id: "BTC/USD".into(),
+        oi_max: dec!(100000),
+        update_price_delta_ratio: dec!(0.005),
+        update_period_seconds: 3600,
+        margin_initial: dec!(0.01),
+        margin_maintenance: dec!(0.005),
+        funding_1: dec!(0.0000000317),
+        funding_2: dec!(0.0000000317),
+        funding_2_delta: dec!(0.000000827),
+        funding_pool_0: dec!(0.0000000159),
+        funding_pool_1: dec!(0.0000000317),
+        funding_share: dec!(0.1),
+        fee_0: dec!(0.0005),
+        fee_1: dec!(0.0000000005),
+    };
+    interface.update_pair_configs(vec![pair_config.clone()]).expect_commit_success();
+
+    let fee_referral_0 = dec!(0.10);
+    let fee_rebate_0 = dec!(0.05);
+    let result_0 = interface.mint_referral(fee_referral_0, fee_rebate_0, 1).expect_commit_success().clone();
+    
+    let referral_id_1 = parse_added_nft_ids(&result_0, referral_resource).first().unwrap().clone();
+    let referral_code_1 = "test".to_string();
+    let referral_hash_1 = keccak256_hash(referral_code_1.clone().into_bytes());
+    let referral_hashes_1 = hashmap!(
+        referral_hash_1 => (vec![], 1u64),
+    );
+    interface.create_referral_codes((referral_resource, referral_id_1), vec![], referral_hashes_1).expect_commit_success();
+
+    let base_input_2 = dec!(100000);
+    interface.add_liquidity((base_resource, base_input_2)).expect_commit_success();
+
+    let base_input_3 = dec!(1000);
+    let result_3 = interface.create_account(
+        rule!(allow_all), 
+        vec![(base_resource, base_input_3)], 
+        Some(referral_code_1),
+    ).expect_commit_success().clone();
+    let margin_account_component = result_3.new_component_addresses()[0];
+
+    let trade_size_4 = dec!(0.02);
+    interface.margin_order_tp_sl_request(
+        0,
+        10000000000,
+        margin_account_component,
+        pair_config.pair_id.clone(),
+        trade_size_4,
+        false,
+        Limit::None,
+        None,
+        None,
+    ).expect_commit_success();
+
+    let price_5 = dec!(60000);
+    let amount_long_5 = dec!(0.2);
+    let amount_short_5 = dec!(0.01);
+    interface.make_open_interest(pair_config.pair_id.clone(), amount_long_5, amount_short_5, price_5);
+
+    let price_6 = dec!(60000);
+    let time_6 = interface.increment_ledger_time(1);
+    interface.process_request(
+        margin_account_component,
+        0, 
+        Some(vec![
+            Price {
+                pair: pair_config.pair_id.clone(),
+                quote: price_6,
+                timestamp: time_6,
+            },
+        ])
+    ).expect_commit_success();
+
+    let price_7 = dec!(75000);
+    let time_7 = interface.increment_ledger_time(10000);
+    interface.auto_deleverage(
+        margin_account_component,
+        pair_config.pair_id.clone(),
+        Some(vec![
+            Price {
+                pair: pair_config.pair_id.clone(),
+                quote: price_7,
+                timestamp: time_7,
+            },
+        ])
+    ).expect_specific_failure(|err| check_error_msg(err, ERROR_ADL_PNL_BELOW_THRESHOLD));
+}
+
+#[test]
+fn test_auto_deleverage_skew_not_reduced() {
+    let mut interface = get_setup();
+    let base_resource = interface.resources.base_resource;
+    let referral_resource = interface.resources.referral_resource;
+    
+    let pair_config_btc = PairConfig {
+        pair_id: "BTC/USD".into(),
+        oi_max: dec!(100000),
+        update_price_delta_ratio: dec!(0.005),
+        update_period_seconds: 3600,
+        margin_initial: dec!(0.01),
+        margin_maintenance: dec!(0.005),
+        funding_1: dec!(0.0000000317),
+        funding_2: dec!(0.0000000317),
+        funding_2_delta: dec!(0.000000827),
+        funding_pool_0: dec!(0.0000000159),
+        funding_pool_1: dec!(0.0000000317),
+        funding_share: dec!(0.1),
+        fee_0: dec!(0.0005),
+        fee_1: dec!(0.0000000005),
+    };
+    let pair_config_xrd = PairConfig {
+        pair_id: "XRD/USD".into(),
+        oi_max: dec!(100000),
+        update_price_delta_ratio: dec!(0.005),
+        update_period_seconds: 3600,
+        margin_initial: dec!(0.01),
+        margin_maintenance: dec!(0.005),
+        funding_1: dec!(0.0000000317),
+        funding_2: dec!(0.0000000317),
+        funding_2_delta: dec!(0.000000827),
+        funding_pool_0: dec!(0.0000000159),
+        funding_pool_1: dec!(0.0000000317),
+        funding_share: dec!(0.1),
+        fee_0: dec!(0.0005),
+        fee_1: dec!(0.0000000005),
+    };
+    interface.update_pair_configs(vec![pair_config_btc.clone(), pair_config_xrd.clone()]).expect_commit_success();
+
+    let fee_referral_0 = dec!(0.10);
+    let fee_rebate_0 = dec!(0.05);
+    let result_0 = interface.mint_referral(fee_referral_0, fee_rebate_0, 1).expect_commit_success().clone();
+    
+    let referral_id_1 = parse_added_nft_ids(&result_0, referral_resource).first().unwrap().clone();
+    let referral_code_1 = "test".to_string();
+    let referral_hash_1 = keccak256_hash(referral_code_1.clone().into_bytes());
+    let referral_hashes_1 = hashmap!(
+        referral_hash_1 => (vec![], 1u64),
+    );
+    interface.create_referral_codes((referral_resource, referral_id_1), vec![], referral_hashes_1).expect_commit_success();
+
+    let base_input_2 = dec!(100000);
+    interface.add_liquidity((base_resource, base_input_2)).expect_commit_success();
+
+    let base_input_3 = dec!(1000);
+    let result_3 = interface.create_account(
+        rule!(allow_all), 
+        vec![(base_resource, base_input_3)], 
+        Some(referral_code_1),
+    ).expect_commit_success().clone();
+    let margin_account_component = result_3.new_component_addresses()[0];
+
+    let trade_size_4 = dec!(-0.02);
+    interface.margin_order_tp_sl_request(
+        0,
+        10000000000,
+        margin_account_component,
+        pair_config_btc.pair_id.clone(),
+        trade_size_4,
+        false,
+        Limit::None,
+        None,
+        None,
+    ).expect_commit_success();
+
+    let btc_price_5 = dec!(60000);
+    let btc_amount_long_5 = dec!(0.05);
+    let btc_amount_short_5 = dec!(0.01);
+    interface.make_open_interest(pair_config_btc.pair_id.clone(), btc_amount_long_5, btc_amount_short_5, btc_price_5);
+    let xrd_price_5 = dec!(0.05);
+    let xrd_amount_long_5 = dec!(150000);
+    let xrd_amount_short_5 = dec!(10000);
+    interface.make_open_interest(pair_config_xrd.pair_id.clone(), xrd_amount_long_5, xrd_amount_short_5, xrd_price_5);
+
+    let btc_price_6 = dec!(60000);
+    let time_6 = interface.increment_ledger_time(1);
+    interface.process_request(
+        margin_account_component,
+        0, 
+        Some(vec![
+            Price {
+                pair: pair_config_btc.pair_id.clone(),
+                quote: btc_price_6,
+                timestamp: time_6,
+            },
+        ])
+    ).expect_commit_success();
+
+    let btc_price_7 = dec!(40000);
+    let xrd_price_7 = dec!(0.1);
+    let time_7 = interface.increment_ledger_time(10000);
+    interface.update_pairs(
+        vec![pair_config_btc.pair_id.clone(), pair_config_xrd.pair_id.clone()],
+        Some(vec![
+            Price {
+                pair: pair_config_btc.pair_id.clone(),
+                quote: btc_price_7,
+                timestamp: time_7,
+            },
+            Price {
+                pair: pair_config_xrd.pair_id.clone(),
+                quote: xrd_price_7,
+                timestamp: time_7,
+            },
+        ]),
+    ).expect_commit_success();
+
+
+    interface.auto_deleverage(
+        margin_account_component,
+        pair_config_btc.pair_id.clone(),
+        None,
+    ).expect_specific_failure(|err| check_error_msg(err, ERROR_ADL_PNL_BELOW_THRESHOLD));
+}
